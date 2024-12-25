5e53a6ed3f4fa8f9285a7e40cab6d1b14d72809c
chienday <144938986+chienday@users.noreply.github.com>
2024-12-25 21:15:37 +0700

    Update App.blade.php

--- resources/views/customers/App.blade.php
+++ resources/views/customers/App.blade.php
@@ -22,6 +22,9 @@
                     <li class="nav-item">
                         <a class="nav-link" href="{{ route('customers.create') }}">Thêm mới</a>
                     </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('product.index') }}">Sản phẩm</a>
+                    </li>
                     
                 </ul>
             </div>
